b25e1f5f28b2c6967d2e9b0cc662eaf33780a777
gbiswa <71508499+gbiswa@users.noreply.github.com>
2021-03-09 20:43:29 -0600

    admin Modification

--- shaveTheDay/Eshop/settings.py
+++ shaveTheDay/Eshop/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_black.apps.AdminBlackConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
