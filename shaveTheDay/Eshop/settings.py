7232c728b5908cad65dcc5ca3ec4edb5c3f70c60
gbiswa <71508499+gbiswa@users.noreply.github.com>
2021-04-02 07:01:48 -0600

    carts

--- shaveTheDay/Eshop/settings.py
+++ shaveTheDay/Eshop/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'store.middlewares.auth.auth_middleware'
+    #'store.middlewares.auth.auth_middleware'
 ]
 
 ROOT_URLCONF = 'Eshop.urls'
